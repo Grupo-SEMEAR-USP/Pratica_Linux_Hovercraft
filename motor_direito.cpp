443f96bf4680349fe98f7092c9a5ef0d0e8026ba
Beatriz Matias <162923385+BeaMatiass@users.noreply.github.com>
2026-06-08 15:06:43 -0300

    Add motor_direito.cpp for motor test output

---
+++ motor_direito.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Teste do motor direito iniciado." << endl;
+    cout << "Motor direito funcionando." << endl;
+    return 0;
+}
